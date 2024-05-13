bd5c7f4a0f095428d7998b86482108b322211b69
Susmita-Dey <susmitadeywork@gmail.com>
2024-05-13 11:17:35 +0530

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -15,18 +15,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-// module.exports = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/blog",
-//         destination: "https://susmitadey.vercel.app/blog",
-//       },
-//       {
-//         source: "/blog/:path*",
-//         destination: "https://susmitadey.vercel.app/blog/:path*",
-//       },
-//     ];
-//   },
-// };
